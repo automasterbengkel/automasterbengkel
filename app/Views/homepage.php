993825d83b09e18a879ee2d051539f75ee9dee54
Automaster Bengkel <websiteautomaster@gmail.com>
2024-01-20 18:30:03 +0700

    first commit

---
+++ app/Views/homepage.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/svg+xml" href="/assets/logo/logoAutomasterBulat.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Automaster Bengkel</title>
+
+    <!-- ----- My CSS ----- -->
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <!-- BOX ICON -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+</head>
+
+<body>
+    <!-- Navbar -->
+    <header>
+        <div class="nav container">
+            <!-- Logo -->
+            <a href="index.html"><img class="logoNav" src="/assets/logo/logoAutomasterBengkel.png" alt=""></a>
+
+            <!-- Menu Icon -->
+            <input type="checkbox" name="" id="menu">
+            <label for="menu"><i class="bx bx-menu" id="menu-icon"></i></label>
+
+            <!-- Nav List -->
+            <ul class="navbar">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#promo">Promo</a></li>
+                <li><a href="#article">Article</a></li>
+                <li><a href="#career">Career</a></li>
+                <li><a href="#shop">Shop</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- Home -->
+    <section class="home container" id="home">
+        <div class="home-text">
+            <h1>Automaster Bengkel</h1>
+            <p>Kami berdiri sejak tahun 2010. Telah banyak pengalaman yang kami dapatkan dalam melayani konsumen untuk memperbaiki kendaraannya. </p>
+            <a href="#" class="btn">Read More</a>
+        </div>
+    </section>
+
+    <!-- About -->
+    <section class="about container" id="about">
+        <div class="about-img">
+            <img src="assets/img/home.jpg" alt="">
+        </div>
+        <div class="about-text">
+            <span>About Us</span>
+            <h2>Sejarah Automaster Bengkel</h2>
+            <p>Kami berdiri sejak tahun 2010. Telah banyak pengalaman yang kami dapatkan dalam melayani konsumen untuk memperbaiki kendaraannya. Berbagai macam kendaraan telah kami tangani dengan baik. Kemajuan teknologi juga tidak kami abaikan begitu saja, karena kami terus mengikuti tuntutan pasar. Kualitas tenaga ahli yang berkompeten adalah keunggulan kami untuk mencapai kepuasan pelanggan. Selain itu, ruang tunggu yang nyaman juga menjadi bentuk bahwa kami terus berkeinginan untuk memfasilitasi pelanggan.</p>
+            <a href="#" class="btn">Read More</a>
+        </div>
+    </section>
+
+    <!-- Services -->
+    <section class="services container" id="services">
+        <h1>Layanan Automaster</h1>
+        <h2>Layanan Terbaik untuk seluruh pelanggan Automaster.</h2>
+        <div class="layanan">
+            <div class="boxServ">
+                <i class='bx bxs-car-wash'></i>
+                <h3>Car Wash</h3>
+                <p>Cuci Mobilmu supaya bersih dan kinclong. Kami pastikan mobilmu menjadi lebih terawat dan kamu pasti lebih nyaman mengendarainya.</p>
+            </div>
+            <div class="boxServ">
+                <i class='bx bxs-car-mechanic'></i>
+                <h3>Bengkel Mobil</h3>
+                <p>Rawat Mobilmu dengan servis mesin, ganti oli, Tune-Up, dll. Servis mobil rutin menjadi sebuah kebiasaan yang harus dilakukan supaya mobilmu tetap nyaman.</p>
+            </div>
+            <div class="boxServ">
+                <i class='bx bx-cable-car'></i>
+                <h3>Spooring & Balancing</h3>
+                <p>Untuk menjaga kestabilan Setir dan Ban mobilmu. Kestabilan Setir dan Ban Mobil menjadi salah satu faktor yang akan membuatmu tetap nyaman berkendara</p>
+            </div>
+            <div class="boxServ">
+                <i class='bx bxs-car-crash'></i>
+                <h3>Body Repair</h3>
+                <p>Benerin mobilmu yang habis tabrakan atau tergores benda tumpul. Jangan biarkan body mobilmu yang tergores dan rusak dibiarkan begitu saja.</p>
+            </div>
+            <div class="boxServ">
+                <i class='bx bxs-car'></i>
+                <h3>Variasi Mobil</h3>
+                <p>Bikin mobilmu lebih berwarna dengan ganti warna cover jok, lampu, klakson, audio, alarm, dan aksesoris lainnya. Kamu bisa berkreasi sesuai dengan apa yang kamu inginkan.</p>
+            </div>
+            <div class="boxServ">
+                <i class='bx bxs-car-garage'></i>
+                <h3>Salon Mobil</h3>
+                <p>Untuk merawat interior dan eksterior mobilmu. Pastikan interior dan eksterior mobilmu tetap terawat dan terhindar dari berbagai masalah.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Posts -->
+    <section class="posts container" id="article">
+        <div class="heading">
+            <span>Recent</span>
+            <h2>Our Feeds</h2>
+            <p>Untuk merawat interior dan eksterior mobilmu. Pastikan interior dan eksterior mobilmu tetap terawat dan terhindar dari berbagai masalah.</p>
+        </div>
+
+        <div class="posts-container container">
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+            </div>
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+            </div>
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+            </div>
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+            </div>
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+            </div>
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+            </div>
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+                <div class="content">
+                </div>
+            </div>
+            <div class="boxPosts">
+                <img src="assets/img/home.jpg" alt="">
+                <h3>20/01/2024</h3>
+            </div>
+        </div>
+    </section>
+
+    <!-- NewsLetter -->
+    <section class="newsletter container">
+        <h2>Have Question in mind?</h2>
+        <p>Let us help you</p>
+        <form action="">
+            <input type="email" name="" id="email-box" placeholder="yourmail@gmail.com" required>
+            <input type="submit" value="Send" class="btn">
+        </form>
+    </section>
+
+    <!-- Sponsor -->
+    <section class="sponsor container">
+        <h2>SPONSOR</h2>
+        <div class="logoSponsor container">
+            <img src="assets/logo/logoBS.png" alt="">
+            <img src="assets/logo/logoAM.png" alt="">
+            <img src="assets/logo/logo3M.png" alt="">
+            <img src="assets/logo/logoAutovision.png" alt="">
+            <img src="assets/logo/logoHurricane.png" alt="">
+            <img src="assets/logo/logoMeguiars.png" alt="">
+            <img src="assets/logo/logoMobil1.png" alt="">
+            <img src="assets/logo/logoSikkens.png" alt="">
+            <img src="assets/logo/logoSonax.png" alt="">
+            <img src="assets/logo/logoVenom.png" alt="">
+            <img src="assets/logo/logoVente.png" alt="">
+        </div>
+
+    </section>
+
+    <!-- Footer -->
+    <section class="footer">
+        <div class="footer-container container">
+            <img src="assets/logo/logoAutomasterBulat.png" alt="">
+            <div class="footer-box">
+                <h3>Quick Links</h3>
+                <a href="#">About Us</a>
+                <a href="#">Promo</a>
+                <a href="#">Article</a>
+                <a href="#">Career</a>
+                <a href="#">Shop</a>
+            </div>
+            <div class="footer-box">
+                <h3>Location</h3>
+                <a href="#">
+                    <h2>Automaster Antapani</h2>
+                    <p>Jl.Terusan Jakarta No.118 Kota Bandung</p>
+                </a>
+                <br>
+                <a href="#">
+                    <h2>Automaster Veteran</h2>
+                    <p>Jl.Veteran No.21 Kota Bandung</p>
+                </a>
+                <br>
+                <a href="#">
+                    <h2>Automaster Bojongsong</h2>
+                    <p>Jl.Terusan Bojongsoang No.97 Kabupaten Bandung</p>
+                </a>
+            </div>
+            <div class="footer-box">
+                <h3>Contact Us</h3>
+                <a href="#">021118118</a>
+                <a href="#">info@automasterbengkel.com</a>
+                <div class="social">
+                    <a href=""><i class="bx bxl-instagram"></i></a>
+                    <a href=""><i class="bx bxl-facebook"></i></a>
+                    <a href=""><i class="bx bxl-twitter"></i></a>
+                    <a href=""><i class="bx bxl-tiktok"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="copyright-content">
+        <h4>Copyright © 2024 Automaster Bengkel</h4>
+        <h4>Designed By <a href="https://instagram.com/dorionsix">
+                NSO Project
+            </a></h4>
+    </div>
+
+</body>
+
+</html>
