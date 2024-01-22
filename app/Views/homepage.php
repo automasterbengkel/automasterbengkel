75f2c6901bb450c406ace7a9b14fc32acf5f00f6
Automaster Bengkel <websiteautomaster@gmail.com>
2024-01-22 13:19:33 +0700

    Landing Page

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -30,7 +30,7 @@
             <ul class="navbar">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#promo">Promo</a></li>
+                <li><a href="#services">Services</a></li>
                 <li><a href="#article">Article</a></li>
                 <li><a href="#career">Career</a></li>
                 <li><a href="#shop">Shop</a></li>
@@ -42,8 +42,9 @@
     <!-- Home -->
     <section class="home container" id="home">
         <div class="home-text">
-            <h1>Automaster Bengkel</h1>
-            <p>Kami berdiri sejak tahun 2010. Telah banyak pengalaman yang kami dapatkan dalam melayani konsumen untuk memperbaiki kendaraannya. </p>
+            <h1>Authorized Dealer</h1>
+            <h1 style="font-size: 44px;">BRIDGESTONE</h1>
+            <p>Pendiri Bridgestone, Shojiro Ishibashi, berusaha menciptakan sebuah perusahaan yang didedikasikan untuk "Menyumbang Masyarakat dengan Mutu Tertinggi." Inilah yang kami perjuangkan setiap hari. </p>
             <a href="#" class="btn">Read More</a>
         </div>
     </section>
@@ -66,36 +67,48 @@
         <h1>Layanan Automaster</h1>
         <h2>Layanan Terbaik untuk seluruh pelanggan Automaster.</h2>
         <div class="layanan">
-            <div class="boxServ">
-                <i class='bx bxs-car-wash'></i>
-                <h3>Car Wash</h3>
-                <p>Cuci Mobilmu supaya bersih dan kinclong. Kami pastikan mobilmu menjadi lebih terawat dan kamu pasti lebih nyaman mengendarainya.</p>
-            </div>
-            <div class="boxServ">
-                <i class='bx bxs-car-mechanic'></i>
-                <h3>Bengkel Mobil</h3>
-                <p>Rawat Mobilmu dengan servis mesin, ganti oli, Tune-Up, dll. Servis mobil rutin menjadi sebuah kebiasaan yang harus dilakukan supaya mobilmu tetap nyaman.</p>
-            </div>
-            <div class="boxServ">
-                <i class='bx bx-cable-car'></i>
-                <h3>Spooring & Balancing</h3>
-                <p>Untuk menjaga kestabilan Setir dan Ban mobilmu. Kestabilan Setir dan Ban Mobil menjadi salah satu faktor yang akan membuatmu tetap nyaman berkendara</p>
-            </div>
-            <div class="boxServ">
-                <i class='bx bxs-car-crash'></i>
-                <h3>Body Repair</h3>
-                <p>Benerin mobilmu yang habis tabrakan atau tergores benda tumpul. Jangan biarkan body mobilmu yang tergores dan rusak dibiarkan begitu saja.</p>
-            </div>
-            <div class="boxServ">
-                <i class='bx bxs-car'></i>
-                <h3>Variasi Mobil</h3>
-                <p>Bikin mobilmu lebih berwarna dengan ganti warna cover jok, lampu, klakson, audio, alarm, dan aksesoris lainnya. Kamu bisa berkreasi sesuai dengan apa yang kamu inginkan.</p>
-            </div>
-            <div class="boxServ">
-                <i class='bx bxs-car-garage'></i>
-                <h3>Salon Mobil</h3>
-                <p>Untuk merawat interior dan eksterior mobilmu. Pastikan interior dan eksterior mobilmu tetap terawat dan terhindar dari berbagai masalah.</p>
-            </div>
+            <a href="" style="color:var(--second-color)">
+                <div class="boxServ">
+                    <i class='bx bxs-car-wash'></i>
+                    <h3>Car Wash</h3>
+                    <p>Cuci Mobilmu supaya bersih dan kinclong. Kami pastikan mobilmu menjadi lebih terawat dan kamu pasti lebih nyaman mengendarainya.</p>
+                </div>
+            </a>
+            <a href="" style="color:var(--second-color)">
+                <div class="boxServ">
+                    <i class='bx bxs-car-mechanic'></i>
+                    <h3>Bengkel Mobil</h3>
+                    <p>Rawat Mobilmu dengan servis mesin, ganti oli, Tune-Up, dll. Servis mobil rutin menjadi sebuah kebiasaan yang harus dilakukan supaya mobilmu tetap nyaman.</p>
+                </div>
+            </a>
+            <a href="" style="color:var(--second-color)">
+                <div class="boxServ">
+                    <i class='bx bx-cable-car'></i>
+                    <h3>Spooring & Balancing</h3>
+                    <p>Untuk menjaga kestabilan Setir dan Ban mobilmu. Kestabilan Setir dan Ban Mobil menjadi salah satu faktor yang akan membuatmu tetap nyaman berkendara</p>
+                </div>
+            </a>
+            <a href="" style="color:var(--second-color)">
+                <div class="boxServ">
+                    <i class='bx bxs-car-crash'></i>
+                    <h3>Body Repair</h3>
+                    <p>Benerin mobilmu yang habis tabrakan atau tergores benda tumpul. Jangan biarkan body mobilmu yang tergores dan rusak dibiarkan begitu saja.</p>
+                </div>
+            </a>
+            <a href="" style="color:var(--second-color)">
+                <div class="boxServ">
+                    <i class='bx bxs-car'></i>
+                    <h3>Variasi Mobil</h3>
+                    <p>Bikin mobilmu lebih berwarna dengan ganti warna cover jok, lampu, klakson, audio, alarm, dan aksesoris lainnya. Kamu bisa berkreasi sesuai dengan apa yang kamu inginkan.</p>
+                </div>
+            </a>
+            <a href="" style="color:var(--second-color)">
+                <div class="boxServ">
+                    <i class='bx bxs-car-garage'></i>
+                    <h3>Salon Mobil</h3>
+                    <p>Untuk merawat interior dan eksterior mobilmu. Pastikan interior dan eksterior mobilmu tetap terawat dan terhindar dari berbagai masalah.</p>
+                </div>
+            </a>
         </div>
     </section>
 
@@ -156,20 +169,24 @@
     </section>
 
     <!-- Sponsor -->
-    <section class="sponsor container">
-        <h2>SPONSOR</h2>
+    <section class="sponsor-container container">
+        <h2 style="text-align: center; margin-bottom:30px">SPONSOR</h2>
         <div class="logoSponsor container">
             <img src="assets/logo/logoBS.png" alt="">
-            <img src="assets/logo/logoAM.png" alt="">
             <img src="assets/logo/logo3M.png" alt="">
             <img src="assets/logo/logoAutovision.png" alt="">
+            <img src="assets/logo/logoVente.png" alt="">
             <img src="assets/logo/logoHurricane.png" alt="">
             <img src="assets/logo/logoMeguiars.png" alt="">
             <img src="assets/logo/logoMobil1.png" alt="">
             <img src="assets/logo/logoSikkens.png" alt="">
             <img src="assets/logo/logoSonax.png" alt="">
             <img src="assets/logo/logoVenom.png" alt="">
-            <img src="assets/logo/logoVente.png" alt="">
+            <img src="assets/logo/logoAM.png" alt="">
+            <img src="assets/logo/logoMichelin.png" alt="">
+            <img src="assets/logo/logoConti.png" alt="">
+            <img src="assets/logo/logoCastrol.png" alt="">
+            <img src="assets/logo/LogoEneos.png" alt="">
         </div>
 
     </section>
@@ -207,11 +224,12 @@
                 <h3>Contact Us</h3>
                 <a href="#">021118118</a>
                 <a href="#">info@automasterbengkel.com</a>
-                <div class="social">
-                    <a href=""><i class="bx bxl-instagram"></i></a>
-                    <a href=""><i class="bx bxl-facebook"></i></a>
-                    <a href=""><i class="bx bxl-twitter"></i></a>
-                    <a href=""><i class="bx bxl-tiktok"></i></a>
+                <div class="social" style="margin-top: 15px;">
+                    <a href="https://www.instagram.com/automasterbengkel/" target="_blank"><i class="bx bxl-instagram" style="color: var(--bg-color); font-size:25px"></i></a>
+                    <a href="https://www.instagram.com/automasterbengkel/" target="_blank"><i class="bx bxl-facebook" style="color: var(--bg-color); font-size:25px"></i></a>
+                    <a href="" target="_blank"><i class="bx bxl-twitter" style="color: var(--bg-color); font-size:25px"></i></a>
+                    <a href="https://www.tiktok.com/@automasterbengkel" target="_blank"><i class="bx bxl-tiktok" style="color: var(--bg-color); font-size:25px"></i></a>
+                    <a href="https://www.youtube.com/@AutomasterTV" target="_blank"><i class="bx bxl-youtube" style="color: var(--bg-color); font-size:25px"></i></a>
                 </div>
             </div>
         </div>
@@ -219,7 +237,7 @@
 
     <div class="copyright-content">
         <h4>Copyright © 2024 Automaster Bengkel</h4>
-        <h4>Designed By <a href="https://instagram.com/dorionsix">
+        <h4>Designed By <a href="https://instagram.com/dorionsix" target="_blank">
                 NSO Project
             </a></h4>
     </div>
